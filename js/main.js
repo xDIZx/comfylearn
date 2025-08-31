b791f2b562f9b94e63f2384f7b07e2797ba2541b
xDIZx <daymdem@gmail.com>
2025-08-31 09:51:48 +0300

    save

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Select all topic names and dropdown lists
+    const topics = document.querySelectorAll(".topic_name");
+
+    topics.forEach(function (topic) {
+        topic.addEventListener("click", function () {
+            // Get the corresponding dropdown list
+            const dropdownList = topic.nextElementSibling;  // Assumes the <ul> is right after the <h2>
+
+            // Toggle the "show" class to show/hide the dropdown
+            dropdownList.classList.toggle("show");
+        });
+    });
+
+
+    // Бургер-меню
+    const burger = document.getElementById("burger");
+    const menu = document.getElementById("menu");
+    const container = document.getElementById("container");
+
+    burger.addEventListener("click", function () {
+        // Перемикаємо клас "open" для бургер-меню
+        burger.classList.toggle("open");
+
+        // Перемикаємо відображення меню та зсув контейнера
+        if (burger.classList.contains("open")) {
+            menu.style.left = "0";
+            container.style.marginLeft = "250px"; // Зсув основного контенту
+        } else {
+            menu.style.left = "-250px";
+            container.style.marginLeft = "0"; // Відновлення положення основного контенту
+        }
+    });
+});
